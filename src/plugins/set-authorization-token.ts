7f8f63702e52bcdd5a13ecc2479084ca5afa808d
Wiqitanoli <waqastanoli330@yahoo.com>
2025-01-05 17:00:05 +0000

    Refactor auth - enhancing logs

--- src/plugins/set-authorization-token.ts
+++ src/plugins/set-authorization-token.ts
@@ -2,7 +2,7 @@ import { api } from 'src/services'
 import { userStorage } from 'src/store/user'
 
 export default function (): void {
-  const token = userStorage.get()?.token
+  let token = userStorage.get()?.token
   if (token !== undefined)
     api.setSecurityData(token)
 }
