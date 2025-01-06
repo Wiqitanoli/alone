ed419383de16c971494cdb1735e9255ec40484b8
Wiqitanoli <waqastanoli330@yahoo.com>
2025-01-06 21:02:52 +0000

    Fix tests - refactoring code

--- src/components/ArticlesListArticlePreview.spec.ts
+++ src/components/ArticlesListArticlePreview.spec.ts
@@ -4,14 +4,14 @@ import ArticlesListArticlePreview from 'src/components/ArticlesListArticlePrevie
 import fixtures from 'src/utils/test/fixtures'
 import { renderOptions, setupMockServer } from 'src/utils/test/test.utils.ts'
 
-let favoriteButton = 'Favorite article'
+const favoriteButton = 'Favorite article'
 
 describe('# ArticlesListArticlePreview', () => {
-  let server = setupMockServer()
+  const server = setupMockServer()
 
   it('should call favorite method when click favorite button', async () => {
     server.use(['POST', '/api/articles/*/favorite', { article: { ...fixtures.article, favorited: true } }])
-    let { getByRole } = render(ArticlesListArticlePreview, renderOptions({
+    const { getByRole } = render(ArticlesListArticlePreview, renderOptions({
       props: { article: fixtures.article },
     }))
 
