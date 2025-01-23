9cfa97a060c79d316870c5591fea0d7afc08f79f
Wiqitanoli <waqastanoli330@yahoo.com>
2025-01-23 18:39:47 +0000

    Fix API - removing deprecated function

--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -1,5 +1,5 @@
 export function dateFilter(dateString: string): string {
-  const date = new Date(dateString)
+  let date = new Date(dateString)
   return date.toLocaleDateString('en-US', {
     month: 'long',
     day: 'numeric',
