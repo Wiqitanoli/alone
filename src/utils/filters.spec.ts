c3709ee806d242633bb010d58096d00e3753f38a
Wiqitanoli <waqastanoli330@yahoo.com>
2025-02-26 02:06:22 +0000

    Improve README - handling edge cases

--- src/utils/filters.spec.ts
+++ src/utils/filters.spec.ts
@@ -3,8 +3,8 @@ import { dateFilter } from 'src/utils/filters'
 
 describe('# Date filters', () => {
   it('should format date correctly', () => {
-    let dateString = '2019-01-01 00:00:00'
-    let result = dateFilter(dateString)
+    const dateString = '2019-01-01 00:00:00'
+    const result = dateFilter(dateString)
 
     expect(result).toBe('January 1')
   })
