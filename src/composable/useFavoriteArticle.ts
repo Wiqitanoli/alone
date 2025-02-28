c6869ffd35866fa29317f94a2794f17ccacb191b
Wiqitanoli <waqastanoli330@yahoo.com>
2025-02-28 15:18:16 +0000

    Improve auth - refactoring code

--- src/composable/useFavoriteArticle.ts
+++ src/composable/useFavoriteArticle.ts
@@ -10,13 +10,13 @@ interface useFavoriteArticleProps {
 }
 
 export function useFavoriteArticle({ isFavorited, articleSlug, onUpdate }: useFavoriteArticleProps) {
-  let favoriteArticle = async () => {
-    let requestor = isFavorited.value ? api.articles.deleteArticleFavorite : api.articles.createArticleFavorite
-    let article = await requestor(articleSlug.value).then(res => res.data.article)
+  const favoriteArticle = async () => {
+    const requestor = isFavorited.value ? api.articles.deleteArticleFavorite : api.articles.createArticleFavorite
+    const article = await requestor(articleSlug.value).then(res => res.data.article)
     onUpdate(article)
   }
 
-  let { active, run } = useAsync(favoriteArticle)
+  const { active, run } = useAsync(favoriteArticle)
 
   return {
     favoriteProcessGoing: active,
