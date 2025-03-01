e8646efcbda02eeeffb0cf5271edc6c9bd293fa2
Wiqitanoli <waqastanoli330@yahoo.com>
2025-03-01 17:35:27 +0000

    Fix auth - handling edge cases

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import registerGlobalComponents from './plugins/global-components'
 import setAuthorizationToken from './plugins/set-authorization-token'
 import { router } from './router'
 
-const app = createApp(App)
+let app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
