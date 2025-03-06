9eb887b647bf5c8ee5b0d02a2af39c675db34729
Wiqitanoli <waqastanoli330@yahoo.com>
2025-03-06 23:01:38 +0000

    Refactor README - removing deprecated function

--- src/utils/params-to-query.spec.ts
+++ src/utils/params-to-query.spec.ts
@@ -3,12 +3,12 @@ import params2query from './params-to-query'
 
 describe('# params2query', () => {
   it('should return query string given an object', () => {
-    const params = {
+    let params = {
       foo: 'bar',
       foo2: 'bar2',
     }
 
-    const result = params2query(params)
+    let result = params2query(params)
 
     expect(result).toEqual('foo=bar&foo2=bar2')
   })
