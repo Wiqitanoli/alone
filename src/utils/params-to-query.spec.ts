ee426cdc82ed676a010f881929c714278f078ff7
Wiqitanoli <waqastanoli330@yahoo.com>
2025-04-01 14:04:28 +0000

    Change configuration - improving performance

--- src/utils/params-to-query.spec.ts
+++ src/utils/params-to-query.spec.ts
@@ -3,12 +3,12 @@ import params2query from './params-to-query'
 
 describe('# params2query', () => {
   it('should return query string given an object', () => {
-    let params = {
+    const params = {
       foo: 'bar',
       foo2: 'bar2',
     }
 
-    let result = params2query(params)
+    const result = params2query(params)
 
     expect(result).toEqual('foo=bar&foo2=bar2')
   })
