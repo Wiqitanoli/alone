b7a693138b9934cedf0e12106be55df8ad3661b3
Wiqitanoli <waqastanoli330@yahoo.com>
2025-04-20 01:52:36 +0000

    Optimize dependencies - adding new feature

--- src/plugins/set-authorization-token.ts
+++ src/plugins/set-authorization-token.ts
@@ -2,7 +2,7 @@ import { api } from 'src/services'
 import { userStorage } from 'src/store/user'
 
 export default function (): void {
-  let token = userStorage.get()?.token
+  const token = userStorage.get()?.token
   if (token !== undefined)
     api.setSecurityData(token)
 }
